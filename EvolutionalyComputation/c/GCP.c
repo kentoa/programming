3b5d70946a281610ca2575f73d7318108c3a8940
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-07-02 03:02:01 +0900

    Graph Coloring Problem

--- EvolutionalyComputation/c/GCP.c
+++ EvolutionalyComputation/c/GCP.c
@@ -1,111 +1,97 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define n 9 // ノード数
-#define M n*(n-1)/4 // 密結合問題
-#define m 3*n // 疎結合問題
+#define N 9 // node size
+#define M N*(N-1)/4 // 密結合問題
+#define m 3*N // 疎結合問題
 #define seed 149 // {149, 151, 157, 163, 167, 173, 179, 181, 191, 193}
 
+void init_graph(int graph[][N]);
+void CSVWrite(char *output, int graph[][N]);
+int GCPGen(int graph[][N], int link);
+
 // Graph Coloring Ploblem
-int main(){ 
-  FILE *fo;
-  char *fname;
-  fname = "GCP.csv";
+int main(){   
+  int matM[N][N]; // 密結合問題を考えるときの行列
+  int matm[N][N]; // 疎結合問題を考えるときの行列
+  
+  init_graph(matM);
+  init_graph(matm);
 
-  if((fo = fopen(fname,"w")) == NULL){
-    printf("File[%s] dose not open!\n",fname);
-    exit(0);
-  }
+  int linkM = GCPGen(matM, M); // 密結合のリンク数  
+  int linkm = GCPGen(matm, m); // 疎結合のリンク数
+    
+  CSVWrite("matM.csv", matM);
+  CSVWrite("matm.csv", matm);
+  
+  printf("密結合問題を考えたときのリンク数は%dです\n", linkM);
+  printf("疎結合問題を考えたときのリンク数は%dです\n", linkm);
  
-  srand(seed);
-  int i, j;
-  int linkM = 0; // 密結合時のリンク数
-  int linkm = 0; // 疎結合時のリンク数
-  int matM[n][n]; // 密結合問題を考えるときの行列
-  int matm[n][n]; // 疎結合問題を考えるときの行列
-  double r; //生成した乱数　
-
-  for(i = 0; i < n; i++){ // step1: n*nの2次元配列を作成(0で初期化)
-    for(j = 0; j < n; j++){
-      matM[i][j] = 0;  
-      matm[i][j] = 0;
-    }
-  }
-        
-  // step2,3: 行列の上三角成分にランダムに1を加えて、密結合問題を考える
-  while(linkM != M){
-    for(i =0; i <n/3; i++)
-      for(j =n/3; j <n; j++){
-	r=(double)rand()/(RAND_MAX+1.0);
-	if(linkM>=M)
-	  break;
-	if(r>=0.5){
-	  if(matM[i][j] ==0){
-	    matM[i][j]=1;
-	    linkM++;
-	  }
-	}
-      }
-    
-    for(i =n/3; i<2*n/3; i++)
-      for(j =2*n/3; j<n; j++){
-	r =(double)rand()/(RAND_MAX+1.0);
-	if(linkM>=M)
-	  break;
-	if(r>=0.5){
-	  if(matM[i][j] ==0){
-	    matM[i][j]=1;
-	    linkM++;
-	  }
-	}
-      }
-  }
-  
-  for(i =0;i<n;i++){
-    for(j=0;j<n;j++)
-      fprintf(fo,"%d,",matM[i][j]);
-    fprintf(fo,"\n");
-  }
-  printf("密結合問題を考えたときのリンク数は%dです\n",linkM);
-  
-    
-  //step2,3:行列の上三角成分にランダムに1を加えて、疎結合問題を考える
-  while(linkm != m){
-    for(i = 0; i < n / 3; i++)
-      for(j = n/3; j < n; j++){
-	r = (double)rand() / (RAND_MAX + 1.0);
-	
-	if(m <= linkm) break;
-	
-	if(0.5 <= r){
-	  if(matm[i][j] == 0){
-	    matm[i][j] = 1;
-	    linkm++;
-	  }
-	}
-      }
-    
-    for(i = n / 3; i < 2 * n / 3; i++)
-      for(j = 2 * n / 3 ; j < n; j++){
-	r = (double)rand() / (RAND_MAX + 1.0);
-	if(linkm >= m) break;
-	if(r >= 0.5){
-	  if(matm[i][j] == 0){
-	    matm[i][j]=1;
-	    linkm++;
-	  }
-	}
-      }
-  }
-  
-  for(i =0;i<n;i++){
-    for(j=0;j<n;j++)
-      fprintf(fo,"%d,",matm[i][j]);
-    fprintf(fo,"\n");
-  }
-  
-  printf("疎結合問題を考えたときのリンク数は%dです\n",linkm);
-  fclose(fo);
-  
   return 0;
 }
+
+void init_graph(int graph[][N]){ // step1: n*nの2次元配列を作成(0で初期化)
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++){
+      graph[i][j] = 0;  
+    }
+  }
+}
+
+int GCPGen(int graph[][N], int link){ // step2,3: 行列の上三角成分にランダムに1を加える
+  srand(seed);
+  int array[N * N / 3];
+  int count = 0, k, r;
+  
+  for(int i = 0, len = N * N / 3; i < len; i++){
+    array[i] = -1;
+  }
+  
+  while(count < link){
+    k = 0;
+    for(int i = 0, row = N / 3; i < row; i++){ // graphとarrayを対応させる
+      for(int j = N / 3; j < N; j++){
+	if(graph[i][j] == 0){ // まだ要素が0のものを格納 
+	  array[k] = j + i * N; 
+	  k++;
+	}
+      }
+    }
+    
+    for(int i = N / 3 , row = 2 * N / 3; i < row; i++){
+      for(int j = 2 * N / 3; j < N; j++){
+	if(graph[i][j] == 0){
+	  array[k] = j + i * N;
+	  k++;
+	}
+      }
+    }
+    
+    r = (int)((double)k * rand() / (RAND_MAX + 1.0)); // 乱数を生成してgraph[][]の上三角成分にランダムに1を代入
+    if(graph[array[r] / N][array[r] % N] != 1){
+      graph[array[r] / N][array[r] % N] = 1;
+      count++;
+    }
+  }
+
+  return count;
+}
+  
+void CSVWrite(char *fname, int graph[][N]){
+  FILE *fo;
+  
+  if((fo = fopen(fname, "w")) == NULL){
+    printf("File[%s] dose not open!\n", fname);
+    exit(0);
+  }
+  
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++){
+      fprintf(fo, "%d,", graph[i][j]);
+    }
+    fprintf(fo, "\n");
+  }  
+  
+  fclose(fo); 
+}
+
