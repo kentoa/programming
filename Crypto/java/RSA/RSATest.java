a7264687c2647eaaf34fcb6bc3db5c2ec75c048b
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-07-16 00:10:17 +0900

    Test enc and dec

---
+++ Crypto/java/RSA/RSATest.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+
+public class RSATest {
+
+    RSATest(){}
+
+    static void RSATest(){
+	BigInteger p = BigInteger.valueOf(5), q = BigInteger.valueOf(7);
+	BigInteger e = BigInteger.valueOf(5), M = BigInteger.valueOf(2);
+	
+	RSAPubKey pk = new RSAPubKey(p, q, e);
+
+	System.out.println("pk: (n, e) = " + "(" + pk.getN() + ", " + pk.getE() + ")");
+	System.out.println("M = " + M);
+
+	BigInteger C = pk.Encrypt(M);
+	
+	System.out.println("C = " + C);
+
+	RSASecKey sk = new RSASecKey(p, q, e); // if e | (p-1)(q-1), error!
+	System.out.println("sk: d = " + sk.getD());
+
+	BigInteger DecC = sk.Decrypt(C);
+	System.out.println("M = " + DecC);
+    }
+
+    public static void main(String[] args){
+	RSATest();
+    }
+}
