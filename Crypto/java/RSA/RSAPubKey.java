5e9cded9470bf2e2808a7bd78def56849789c66a
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-07-15 14:17:28 +0900

    delete RSAPubKeyTest() and main()

--- Crypto/java/RSA/RSAPubKey.java
+++ Crypto/java/RSA/RSAPubKey.java
@@ -1,6 +1,5 @@
 import java.math.BigInteger;
 
-
 public class RSAPubKey {
     private BigInteger n, e; // public key
     
@@ -18,24 +17,7 @@ public class RSAPubKey {
     }
 
     public BigInteger Encrypt(BigInteger m){
-	BigInteger c;
-	c = m.modPow(this.e, this.n); 
+	BigInteger c = m.modPow(this.e, this.n); 
 	return c; 
-    }
-    
-    public static void RSAPubKeyTest(){
-	BigInteger p = BigInteger.valueOf(5), q = BigInteger.valueOf(7);
-	BigInteger e = BigInteger.valueOf(2), m = BigInteger.valueOf(3);
-	RSAPubKey pk = new RSAPubKey(p, q, e);
-	
-	System.out.println("(n, e) = " + "(" + pk.getN() + ", " + pk.getE() + ")");
-	
-	BigInteger c = pk.Encrypt(m);
-	
-	System.out.println("c = " + c);
-    }
-    
-    public static void main(String[] args){
-	RSAPubKeyTest();
     }
 }
