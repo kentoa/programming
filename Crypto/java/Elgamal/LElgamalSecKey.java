6b0f3e87aa4990b8abb528b76676c16524d16f68
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-07-23 02:06:17 +0900

    復号できるように修正

--- Crypto/java/Elgamal/LElgamalSecKey.java
+++ Crypto/java/Elgamal/LElgamalSecKey.java
@@ -9,22 +9,21 @@ public class LElgamalSecKey {
 
     BigInteger Decrypt(BigInteger[] c, BigInteger p, BigInteger h){
 	BigInteger hm = BigInteger.ZERO;
-	BigInteger c1x = c[0].modPow(this.x, p);
+	BigInteger c0x = c[0].modPow(this.x, p);
 	
 	try{
-	    hm = (c[1].multiply(c1x.modInverse(p))).mod(p);
+	    hm = (c[1].multiply(c0x.modInverse(p))).mod(p);
 	}catch(Exception e){
 	    System.out.println("inverse not found");
 	    System.exit(1);
 	}
 	
 	BigInteger m = BigInteger.ONE, tmp_h = h;
-
+	
 	while(tmp_h.compareTo(hm) != 0){
-	    tmp_h.multiply(h);
-	    m.add(BigInteger.ONE);
+	    tmp_h = tmp_h.multiply(h).mod(p);
+	    m = m.add(BigInteger.ONE);
 	}
-
 	return m;
     }
 }
