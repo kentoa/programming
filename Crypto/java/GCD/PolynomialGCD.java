de60754af4fac6ae7a82e4b8e0a10cb80d75883a
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-07-27 21:39:37 +0900

    GCD for polynomial

---
+++ Crypto/java/GCD/PolynomialGCD.java
@@ -0,0 +1,17 @@
+
+public class PolynomialGCD {
+
+    ArrayList<Integer> poly_gcd(ArrayList<Integer> a, ArrayList<Integer> b){
+	ArrayList<Integer> res;
+	int len_a = a.size();
+	int len_b = b.size();	
+
+	if(len_a < len_b){
+	    
+	}
+	
+	return res;
+    }
+
+
+}
