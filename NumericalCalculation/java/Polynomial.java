50b719d667f497c1e5413f16d0829105a78d6432
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-08-08 11:55:21 +0900

    add AddCoeff()

--- NumericalCalculation/java/Polynomial.java
+++ NumericalCalculation/java/Polynomial.java
@@ -3,47 +3,72 @@ import java.util.ArrayList;
 public class Polynomial {
     private ArrayList<Integer> coeff = new ArrayList<>(); // f(x) = c0 + c1*x + c2*x^2 + ...
 
-    public void SetCoeff(ArrayList<Integer> coeff){
-	int len = coeff.size();
-	
-	for(int i = 0; i < len; i++){
-	    this.coeff.add(coeff.get(i));
+    public Polynomial(){
+
+    }
+
+    public Polynomial(int[] coeff){
+	for(int i = 0, len = coeff.length; i < len; i++){
+	    setCoeff(coeff[i]);
 	}
     }
 
-    public void SetCoeff(int[] coeff){
-	int len = coeff.length;
-	
-	for(int i = 0; i < len; i++){
-	    this.coeff.add(coeff[i]);
+    public Polynomial(ArrayList<Integer> coeff){
+	for(int i = 0, len = coeff.size(); i < len; i++){
+	    setCoeff(coeff.get(i));
 	}
+    }
+
+    public void setCoeff(int coeff){
+	this.coeff.add(coeff);
     }
 
     public int getVal(int x){
-	int tmp = 1, fx = 0, deg = this.coeff.size();
-	ArrayList<Integer> PowerOfX = new ArrayList<>();
+	int tmp = 1, fx = 0, deg = getDegree();
+	ArrayList<Integer> xval = new ArrayList<>();
 
 	for(int i = 0; i < deg; i++){
-	    PowerOfX.add(tmp);
+	    xval.add(tmp);
 	    tmp *= x;
 	}
 
 	for(int i = 0; i < deg; i++){
-	    fx += this.coeff.get(i) * PowerOfX.get(i); 
+	    fx += this.coeff.get(i) * xval.get(i); 
 	}
 	return fx;
     }
 
     public int getCoeffAt(int i){
-	int ci = this.coeff.get(i); 
-	return ci;
+	int c = this.coeff.get(i); 
+	return c;
     }
+
+    public int getDegree(){
+	int deg = this.coeff.size();
+	return deg;
+    }    
 
     public Polynomial AddCoeff(Polynomial coeffA, Polynomial coeffB){
 	Polynomial coeff = new Polynomial();
-
-
+    
+	int degA = coeffA.getDegree(), degB = coeffB.getDegree();
+    
+	if(degA < degB){
+	    for(int i = degA; i < degB; i++){
+		coeffA.setCoeff(0);
+	    }
+	}
+	else if(degB < degA){
+	    for(int i = degB; i < degA; i++){
+		coeffB.setCoeff(0);
+	    }
+	}
+    
+	int newdeg = coeffA.getDegree(); 
+    
+	for(int i = 0; i < newdeg; i++){
+	    coeff.setCoeff(coeffA.getCoeffAt(i) + coeffB.getCoeffAt(i));
+	}    
 	return coeff;
     }
-
 }
