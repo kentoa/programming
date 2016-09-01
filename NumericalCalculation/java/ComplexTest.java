8b411eed4f57623f1057e5a74ed9c3ec0c2cc905
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-09-01 21:56:02 +0900

    add absTest()

--- NumericalCalculation/java/ComplexTest.java
+++ NumericalCalculation/java/ComplexTest.java
@@ -8,7 +8,31 @@ public class ComplexTest {
 	c.print();
     }
 
+    static void subTest(){
+	Complex c = c1.sub(c2);
+	c.print();
+    }
+
+    static void mulTest(){
+	Complex c = c1.mul(c2);
+	c.print();
+    }
+
+    static void divTest(){
+	Complex c = c1.div(c2);
+	c.print();
+    }
+
+    static void absTest(){
+	double abs = c1.abs();
+	System.out.println("abs(c1) = " + abs);
+    }
+
     public static void main(String[] args){
-	addTest();
+	// addTest();
+	// subTest();
+	// mulTest();
+	// divTest();
+	absTest();
     }
 }
