bed21dcc60c122d84a639b3c934ced5838a0d21a
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-09-07 21:09:50 +0900

    add scanCity(), setCityNum()

--- EvolutionalyComputation/java/TSP_OPT/GA.java
+++ EvolutionalyComputation/java/TSP_OPT/GA.java
@@ -7,32 +7,48 @@ import java.util.ArrayList;
 public class GA {
     private double city[][];
     private double dist[][];
+    private double solution[]; // city path
+    private int cityNum;
     
     GA(){
 	
     }
 
-    void initCity2D(String filepath){
+    ArrayList<String[]> scanCity(String filepath){
 	File file = new File(filepath);
-	
+ 
 	try{
 	    FileInputStream fis = new FileInputStream(file);
 	    InputStreamReader isr = new InputStreamReader(fis);
 	    BufferedReader br = new BufferedReader(isr);
 	    String line;
-	    ArrayList xy = new ArrayList();
+	    private ArrayList point = new ArrayList(); // (x, y)
 
 	    while((line = br.readLine()) != null){
-		String[] point = line.split(",");
-		xy.add(point);
+		String[] tmp = line.split(",");
+		point.add(tmp);
+	    }
 	}
+	return point;
+    }
 
-	    int row = xy.size();
-	    city = new double[row][2];
+    void setCityNum(ArrayList<String[]> point){
+	this.cityNum = point.size();
+    }
+
+    void initCity2D(ArrayList<String[]> point){
+	city = new double[cityNum][3];
 	    
-	    for(int i = 0; i < row; i++){
-		city[i][0] = Integer.valueOf(xy.get(i)[0]);
-		city[i][1] = Integer.valueOf(xy.get(i)[1]);
+	for(int i = 0; i < cityNum; i++){
+	    city[i][0] = i; // city num
+	    city[i][1] = Integer.valueOf(xy.get(i)[0]); // x
+	    city[i][2] = Integer.valueOf(xy.get(i)[1]); // y
+	}
+    }  
+
+	void calcDist(){
+	    for(int i = 0; i < cityNum; i++){
+
 	    }
 	}
 	
