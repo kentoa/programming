58c9f8958372977bdbd4e7a4aef6849b729b7efb
kentoa <k_akiyama@kde.cs.tsukuba.ac.jp>
2016-09-14 21:16:11 +0900

    add keyGen(), Decrypt(), lcm()

--- Crypto/java/Paillier/PaillierSecKey.java
+++ Crypto/java/Paillier/PaillierSecKey.java
@@ -11,17 +11,42 @@ public class PaillierSecKey {
     private BigInteger g;
     private BigInteger squareN;
 
-    PaillierSecKey(BigInteger bit){
-	long seed = 149L;
-	Random rnd = new Random(seed);
-
-	this.p = BigInteger.probablePrime(bit, rnd);
-	this.q = BigInteger.probablePrime(bit, rnd);
+    PaillierSecKey(int bit){
+	keyGen(bit);
+	this.lambda = lcm(p.subtact(BigInteger.ONE), q.subtract(BigInteger.ONE));
 	this.n = p.multiply(q);
 	this.squareN = n.multiply(n);
     }
 
+    void keyGen(int bit){
+	long seed = 149L;
+	Random rnd = new Random(seed);
+	
+	this.p = BigInteger.probablePrime(bit, rnd);
+	this.q = BigInteger.probablePrime(bit, rnd);
+    }
     
+    BigInteger Decrypt(BigInteger c){
+	BigInteger m;
+	BigInteger fc = c.modPow(lambda, squareN);
+	BigInteger fg = g.modPow(lambda, squareN);
+	
+	fc = fc.subtract(BigInteger.ONE);
+	fg = fg.subtract(BigInteger.ONE);	
+	fc = fc.divide(n);
+	fg = fg.divide(n);
 
+	m = fc.multiply(fg.modInverse(n));
+	m = m.mod(n);
+
+	return m;
+    }    
+
+    BigInteger lcm(BigInteger p, BigInteger q){
+        BigInteger pq = p.multiply(q);
+	BigInteger gcd = p.gcd(q);
+
+	return pq.divide(gcd);
+    }
 
 }
